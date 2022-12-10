d750074fe79b214cc7a0229e34adca58fb90d607
ZrC <94488901+AS-Coope@users.noreply.github.com>
2022-12-10 12:01:26 -0800

    Make Habit Component Update
    Detailed Make Habit Screen (more additions to come)

--- app/src/main/java/com/example/swen3001routinehabittracker/MainActivity.kt
+++ app/src/main/java/com/example/swen3001routinehabittracker/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        /*
         val button1 = findViewById<Button>(R.id.button1)
         val button2 = findViewById<Button>(R.id.button2)
 
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,CreateHabitScreen::class.java)
             startActivity(intent)
         }
-        /* Button intent to go to the calendar view
+         Button intent to go to the calendar view
         button2.setOnClickListener {
             val intent = Intent(this,CreateHabitScreen::class.java)
             startActivity(intent)
